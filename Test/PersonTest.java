67167bf0a614765851575e437af2c4df4eb204db
Zevez7 <datguy7@gmail.com>
2022-01-20 02:30:23 -0500

    add comments to all the test function

--- Test/PersonTest.java
+++ Test/PersonTest.java
@@ -13,26 +13,39 @@ public class PersonTest {
 
   private Person john;
 
+  /**
+   * create the person class
+   */
   @Before
   public void setUp() {
 
     john = new Person("John", "Doe", 1945);
   }
 
+  /**
+   * test the function getFirstName
+   */
+
   @Test
   public void testFirst() {
     assertEquals("John", john.getFirstName());
-
   }
+
+  /**
+   * test the function getLastName
+   */
 
   @Test
   public void testSecond() {
     assertEquals("Doe", john.getLastName());
   }
 
+  /**
+   * test the function getYearOfBirth
+   */
+
   @Test
   public void testYearOfBirth() {
     assertEquals(1945, john.getYearOfBirth());
   }
-
 }
